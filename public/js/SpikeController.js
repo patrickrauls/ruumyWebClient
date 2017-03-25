37aa5c11015318c34bd11a773a2375e15647bbe2
Patrick Rauls <patrickrauls@Patricks-Air.domain>
2017-03-25 00:49:19 -0600

    add to cart

--- public/js/SpikeController.js
+++ public/js/SpikeController.js
@@ -1,18 +1,31 @@
 function SpikeController() {
+    this.cart = [];
     this.products = [
         {
             title: 'ruumy',
-            price: 7
+            stock: 25,
+            quantity: 1,
+            price: 7,
+            featured: true
         },
         {
             title: 'large',
+            stock: 2,
+            quantity: 1,
             price: 9
         },
         {
             title: 'small',
+            stock: 0,
+            quantity: 1,
             price: 5
         }
     ]
+    this.addToCart = function (item) {
+        this.cart.includes(item) ?
+            this.cart[this.cart.indexOf(item)].quantity++ :
+            this.cart.push(item)
+    }
 }
 
 angular
